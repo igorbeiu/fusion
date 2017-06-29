068160e16f658b2dc070690bfaed3bcf397abf26
asynchorswim <seanphawkins@gmail.com>
2017-06-29 09:35:27 -0400

    Add bookmark request response message

--- src/main/scala/asynchorswim/fusion/util/Bookmarks.scala
+++ src/main/scala/asynchorswim/fusion/util/Bookmarks.scala
@@ -3,5 +3,7 @@ package asynchorswim.fusion.util
 object Bookmarks {
   final case class Get(topic: String)
   final case class Put[A](topic: String, offset: A)
+  
+  final case class BookmarkValue[A](value: A)
 }
 
