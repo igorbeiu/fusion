d08137607ea6b01901bf2675479853f06ee81781
asynchorswim <seanphawkins@gmail.com>
2017-08-08 08:34:45 -0400

    Settable ordering in EntityRefFlow

--- src/main/scala/asynchorswim/fusion/EntityRefFlow.scala
+++ src/main/scala/asynchorswim/fusion/EntityRefFlow.scala
@@ -6,8 +6,14 @@ import akka.stream.scaladsl.Flow
 import akka.util.Timeout
 import asynchorswim.fusion.ControlMessages._
 
-class EntityRefFlow(target: ActorRef, parallelizm: Int = 1)(implicit timeout: Timeout) {
-  val flow = Flow[StreamCommandEnvelope[_]]
-               .mapAsync[Seq[StreamEventEnvelope[_]]](parallelizm)(c => (target ? c).mapTo[Seq[StreamEventEnvelope[_]]])
-               .mapConcat[StreamEventEnvelope[_]](_.toList)
+class EntityRefFlow(target: ActorRef, parallelizm: Int = 1, maintainOrder: Boolean = true)(implicit timeout: Timeout) {
+  val flow =
+    if (maintainOrder) 
+      Flow[StreamCommandEnvelope[_]]
+        .mapAsync[Seq[StreamEventEnvelope[_]]](parallelizm)(c => (target ? c).mapTo[Seq[StreamEventEnvelope[_]]])
+        .mapConcat[StreamEventEnvelope[_]](_.toList)
+    else
+      Flow[StreamCommandEnvelope[_]]
+        .mapAsyncUnordered[Seq[StreamEventEnvelope[_]]](parallelizm)(c => (target ? c).mapTo[Seq[StreamEventEnvelope[_]]])
+        .mapConcat[StreamEventEnvelope[_]](_.toList)
 }
