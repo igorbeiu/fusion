9b3df1edd2e4d9d53a53f084210fa2a9ec7c3cad
asynchorswim <seanphawkins@gmail.com>
2017-08-08 08:28:44 -0400

    Configurable paralellizm for EntityRefFlow

--- src/main/scala/asynchorswim/fusion/EntityRefFlow.scala
+++ src/main/scala/asynchorswim/fusion/EntityRefFlow.scala
@@ -6,8 +6,8 @@ import akka.stream.scaladsl.Flow
 import akka.util.Timeout
 import asynchorswim.fusion.ControlMessages._
 
-class EntityRefFlow(target: ActorRef)(implicit timeout: Timeout) {
+class EntityRefFlow(target: ActorRef, parallelizm: Int = 1)(implicit timeout: Timeout) {
   val flow = Flow[StreamCommandEnvelope[_]]
-               .mapAsync[Seq[StreamEventEnvelope[_]]](1)(c => (target ? c).mapTo[Seq[StreamEventEnvelope[_]]])
+               .mapAsync[Seq[StreamEventEnvelope[_]]](parallelizm)(c => (target ? c).mapTo[Seq[StreamEventEnvelope[_]]])
                .mapConcat[StreamEventEnvelope[_]](_.toList)
 }
