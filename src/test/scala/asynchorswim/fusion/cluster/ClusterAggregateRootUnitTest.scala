2e51c6a943fe026b4edcd7208515d92a2418779d
Sean P. Hawkins <seanphawkins@gmail.com>
2017-08-13 14:18:11 -0400

    Cluster Aggregate test fix Issue #2

--- src/test/scala/asynchorswim/fusion/cluster/ClusterAggregateRootUnitTest.scala
+++ src/test/scala/asynchorswim/fusion/cluster/ClusterAggregateRootUnitTest.scala
@@ -8,7 +8,6 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.{AsyncFlatSpec, BeforeAndAfterAll, Matchers}
 import akka.pattern.ask
 import asynchorswim.fusion._
-import asynchorswim.fusion.cluster.WordCount.Count
 
 import concurrent.duration._
 import language.postfixOps
@@ -21,10 +20,10 @@ class ClusterAggregateRootUnitTest extends AsyncFlatSpec with Matchers with Befo
        |    provider = "cluster"
        |  }
        |  remote {
-       |    log-remote-lifecycle-events = off
-       |    netty.tcp {
-       |      hostname = "127.0.0.1"
-       |      port = 2555
+       |    artery {
+       |      enabled = on
+       |      canonical.hostname = "127.0.0.1"
+       |      canonical.port = 25520
        |    }
        |  }
        |  persistence {
@@ -32,41 +31,7 @@ class ClusterAggregateRootUnitTest extends AsyncFlatSpec with Matchers with Befo
        |    snapshot-store.plugin = "inmemory-snapshot-store"
        |  }
        |  cluster {
-       |    seed-nodes = ["akka.tcp://TestSystem@127.0.0.1:2555"]
-       |    sharding {
-       |      guardian-name = sharding
-       |      role = ""
-       |      remember-entities = off
-       |      coordinator-failure-backoff = 5 s
-       |      retry-interval = 2 s
-       |      buffer-size = 100000
-       |      handoff-timeout = 60 s
-       |      shard-start-timeout = 10 s
-       |      shard-failure-backoff = 10 s
-       |      entity-restart-backoff = 10 s
-       |      rebalance-interval = 10 s
-       |      journal-plugin-id = ""
-       |      snapshot-plugin-id = ""
-       |      state-store-mode = "ddata"
-       |      snapshot-after = 1000
-       |      keep-nr-of-batches = 2
-       |      least-shard-allocation-strategy {
-       |        rebalance-threshold = 10
-       |        max-simultaneous-rebalance = 3
-       |      }
-       |      waiting-for-state-timeout = 5 s
-       |      updating-state-timeout = 5 s
-       |      entity-recovery-strategy = "all"
-       |      entity-recovery-constant-rate-strategy {
-       |        frequency = 100 ms
-       |        number-of-entities = 5
-       |      }
-       |      distributed-data {
-       |        majority-min-cap = 5
-       |        durable.keys = ["shard-*"]
-       |        akka.cluster.sharding.distributed-data.max-delta-elements = 5
-       |      }
-       |    }
+       |    seed-nodes = ["akka://ClusterAggregateRootUnitTestSystem@127.0.0.1:25520"]
        |  }
        |}
        |inmemory-journal {
@@ -84,46 +49,45 @@ class ClusterAggregateRootUnitTest extends AsyncFlatSpec with Matchers with Befo
        |}
      """.stripMargin
   private val config = ConfigFactory.parseString(configString)
-  private val system = ActorSystem("ClusterTestSystem", config)
+  private val system = ActorSystem("ClusterAggregateRootUnitTestSystem", config)
   private val now = Instant.now
-  private implicit val fc = FusionConfig(new FixedTimeProvider(now), asyncIO = false)
+  private implicit val fc = FusionConfig(new FixedTimeProvider(now))
   private implicit val timeout = Timeout(10 second)
-  private val sut = system.actorOf(ClusterAggregateRoot.props[WordCount](TransientEntity, "WordCount"), "Words")
+  private val sut = system.actorOf(ClusterAggregateRoot.props[ClusterWordCount](TransientEntity, "ClusterWordCount"), "clusterWords")
 
   "ClusterAggregateRoot" should "create entities correctly" in {
     "The rain in Spain falls mainly in the plain"
       .split(" ")
-      .map(_.toUpperCase()) foreach { w => sut ! Count(w) }
-    (sut ? Query("THE")) map { _ shouldBe 2 }
-    (sut ? Query("SPAIN")) map { _ shouldBe 1 }
-    (sut ? Query("XXX")) map { _ shouldBe 0 }
+      .map(_.toUpperCase()) foreach { w => sut ! ClusterWordCount.Count(w) }
+    (sut ? ClusterWordCount.Query("THE")) map { _ shouldBe 2 }
+    (sut ? ClusterWordCount.Query("SPAIN")) map { _ shouldBe 1 }
+    (sut ? ClusterWordCount.Query("XXX")) map { _ shouldBe 0 }
   }
 
   it should "handle Expunge correctly" in {
     sut ! ControlMessages.Expunge("THE")
     Thread.sleep(500)
-    (sut ? Query("THE")) map { _ shouldBe 0 }
+    (sut ? ClusterWordCount.Query("THE")) map { _ shouldBe 0 }
   }
 
   override def afterAll(): Unit = system.terminate()
 
 }
 
-case class WordCount(count: Int) extends Entity[WordCount] {
-  import WordCount._
+case class ClusterWordCount(count: Int) extends Entity[ClusterWordCount] {
 
   override def receive(implicit ctx: Context): PartialFunction[Any, Seq[Event]] = {
-    case Count(w) => applying(Increment)
-    case Query(w) => reply(count); NoOp
+    case ClusterWordCount.Count(w) => applying(ClusterWordCount.Increment)
+    case ClusterWordCount.Query(w) => reply(count); NoOp
   }
 
-  override def applyEvent: PartialFunction[Event, WordCount] = {
-    case Increment => copy( count = count + 1)
+  override def applyEvent: PartialFunction[Event, ClusterWordCount] = {
+    case ClusterWordCount.Increment => copy( count = count + 1)
   }
 }
 
-object WordCount extends EntityCompanion[WordCount] {
-  val empty = WordCount(0)
+object ClusterWordCount extends EntityCompanion[ClusterWordCount] {
+  val empty = ClusterWordCount(0)
 
   case class Count(id: String) extends ShardingId with Command
   case class Query(id: String) extends ShardingId
